d51176129792c6ad065b66e040a8759a05f3a1f5
AndersonLS <indiedev.core@gmail.com>
2026-09-10 15:56:31 -0300

    Initial commit

--- app/src/main/java/com/proflucas/myapplication/CelsiusActivity.kt
+++ app/src/main/java/com/proflucas/myapplication/CelsiusActivity.kt
@@ -18,48 +18,54 @@ class CelsiusActivity : ComponentActivity() {
 
         Toast.makeText(this, "Trocou para Celsius!", Toast.LENGTH_SHORT).show()
 
-        val txtCelsius = findViewById<EditText>(R.id.txtCelsius)
+        val txtPeso = findViewById<EditText>(R.id.txtPeso)
+        val txtAltura = findViewById<EditText>(R.id.txtAltura)
+
+        val btnCalcularCelsius = findViewById<Button>(R.id.btnCalcularIMC)
+
         val labelConversor = findViewById<TextView>(R.id.labelConversor)
-        val btnCalcularCelsius = findViewById<Button>(R.id.btnCalcularCelsius)
 
         btnCalcularCelsius.setOnClickListener {
-            // Pegar o texto do EditText
-            val input = txtCelsius.text.toString().trim()
 
-            if (input.isEmpty()) {
-                Toast.makeText(this, "Digite uma temperatura!", Toast.LENGTH_SHORT).show()
+            val peso = txtPeso.text.toString().toFloatOrNull()
+            val altura = txtAltura.text.toString().toFloatOrNull()
+
+            if (peso ==  null || altura == null) {
+                Toast.makeText(this, "Os dois campos precisam ser preenchidos", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             try {
-                val celsius = input.toDouble()
-
-                // Fórmula: °F = (°C × 9/5) + 32
-                val fahrenheit = (celsius * 9.0 / 5.0) + 32.0
-
-                // Mostrar resultado com 1 casa decimal
-                labelConversor.text = String.format("Resultado: %.1f °F", fahrenheit)
+                val IMC = peso / ((altura / 100) * ( altura / 100));
+                IMCIndicator(IMC, labelConversor)
 
             } catch (e: NumberFormatException) {
                 Toast.makeText(this, "Digite um número válido!", Toast.LENGTH_SHORT).show()
-                labelConversor.text = "Resultado: — °F"
+                labelConversor.text = "O valor digitado não é valido"
             }
         }
 
         val btnSwitchToMedia = findViewById<Button>(R.id.btnLayoutMedia)
-        val btnSwitchToParImpar = findViewById<Button>(R.id.btnLayoutParImpar)
 
         btnSwitchToMedia.setOnClickListener {
             switchToLayoutMedia()
-        }
-
-        btnSwitchToParImpar.setOnClickListener {
-            startActivity(Intent(this, ParImparActivity::class.java))
         }
     }
 
     private fun switchToLayoutMedia() {
         startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    private fun  IMCIndicator(IMC: Float, label: TextView)
+    {
+        when {
+            IMC <= 18.5 -> label.text = " Seu peso esta abaixo do ideal para sua idade\n" + "IMC: %.2f".format(IMC)
+            IMC <= 25 -> label.text ="Seu peso esta ideal, não se preocupe\n" + "IMC: %.2f".format(IMC)
+            IMC <= 30 -> label.text ="Seu peso esta acima do ideal\n" + "IMC: %.2f".format(IMC)
+            IMC <= 35 -> label.text ="Voce sofre de obesidade grau I, procure um nutricionista\n" + "IMC: %.2f".format(IMC)
+            IMC <= 40 -> label.text ="Voce sofre de obesidade grau II, procure um nutricionista o mais rapido possivel\n" + "IMC: %.2f".format(IMC)
+            else -> label.text = "Voce sofre de obesidade grau III, procure um nutricionista e um medico o mais rapido possivel.\n" + "IMC: %.2f".format(IMC)
+        }
     }
 
 }
--- app/src/main/java/com/proflucas/myapplication/MainActivity.kt
+++ app/src/main/java/com/proflucas/myapplication/MainActivity.kt
@@ -22,36 +22,36 @@ class MainActivity : ComponentActivity() {
 
         val txtNota1 = findViewById<EditText>(R.id.txtNota1)
         val txtNota2 = findViewById<EditText>(R.id.txtNota2)
+        val txtNota3 = findViewById<EditText>(R.id.txtNota3)
+        val txtNota4 = findViewById<EditText>(R.id.txtNota4)
         val labelMedia = findViewById<TextView>(R.id.labelMedia)
         val btnCalcularMedia = findViewById<Button>(R.id.btnCalcularMedia)
 
         btnCalcularMedia.setOnClickListener {
             val nota1 = txtNota1.text.toString().toDoubleOrNull()
             val nota2 = txtNota2.text.toString().toDoubleOrNull()
-            if (nota1 == null || nota2 == null) {
+            val nota3 = txtNota3.text.toString().toDoubleOrNull()
+            val nota4 = txtNota4.text.toString().toDoubleOrNull()
+
+            if (nota1 == null || nota2 == null || nota3 == null || nota4 == null) {
                 labelMedia.text = "Nota Inválida"
                 return@setOnClickListener
             }
 
-            val media: Double? = (nota1 + nota2) / 2
+            val media: Double? = (nota1 + nota2 + nota3 + nota4) / 4
             labelMedia.text = "Média: $media"
 
         }
 
         // Configurar o botão no layout media
-        val btnSwitchToCelsius = findViewById<Button>(R.id.btnLayoutCelsius)
-        val btnSwitchToParImpar = findViewById<Button>(R.id.btnLayoutParImpar)
+        val btnSwitchToIMCCalculator = findViewById<Button>(R.id.btnLayoutMedia)
 
-        btnSwitchToCelsius.setOnClickListener {
-            switchToLayoutCelsius()
-        }
-
-        btnSwitchToParImpar.setOnClickListener {
-            startActivity(Intent(this, ParImparActivity::class.java))
+        btnSwitchToIMCCalculator.setOnClickListener {
+            switchToIMCCalculator()
         }
     }
 
-    private fun switchToLayoutCelsius() {
+    private fun switchToIMCCalculator() {
         startActivity(Intent(this, CelsiusActivity::class.java))
     }
 
